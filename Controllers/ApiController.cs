de851fae2e8f967bb50e48da75c6000479d12ecf
vlad-boroda <enity.w@gmail.com>
2021-09-10 00:36:19 +0300

    init

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ZhoskiyBenchSharp.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet("plaintext")]
+        public string PlainText()
+        {
+            return "ok lets go";
+        }
+    }
+}
