a83afc8d2cbbf9a43b5e6c30134233ec1bafdb80
vlad-boroda <enity.w@gmail.com>
2021-09-10 01:09:23 +0300

    database integration 2

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using ZhoskiyBenchSharp.Models;
 
 namespace ZhoskiyBenchSharp.Controllers
 {
@@ -11,10 +13,23 @@ namespace ZhoskiyBenchSharp.Controllers
     [Route("api")]
     public class ApiController : ControllerBase
     {
+        private readonly AppContext _context;
+
+        public ApiController(AppContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet("plaintext")]
         public string PlainText()
         {
             return "ok lets go";
+        }
+        
+        [HttpGet("read-data")]
+        public async Task<List<Bear>> ReadData()
+        {
+            return await _context.Bears.ToListAsync();
         }
     }
 }
